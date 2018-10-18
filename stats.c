f77c04cdb6287ed4983926bfb6715da0cb448e7a
muhammed <muhammedkhaled1110@gmail.com>
2018-10-18 16:25:47 +0200

    Adding code outline and documentation for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -9,14 +9,12 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.c> 
+ * @brief this source file contains a main function along side 7 functions' definitions used to extract important stats of a data
+ * set which in our case an array of 40 chars.
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
- *
+ * @author Muhammed Khaled
+ * @date   8:08AM PDT 10/18/2018
  */
 
 
@@ -29,15 +27,63 @@
 
 void main() {
 
+  
+
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+  
 
 }
 
-/* Add other Implementation File Code Here */
+/**************************************Functions' Definitions***************************************/
+void print_statistics(char * data, unsigned int size)
+{
+
+	
+
+}
+/***************************************************************/
+void print_array(char * data, unsigned int size)
+{
+
+	
+
+}
+/**************************************************************/
+unsigned char find_median(char * data, unsigned int size)
+{
+
+
+
+}
+/*************************************************************/
+unsigned char find_mean(char * data, unsigned int size)
+{
+
+
+
+}
+/**************************************************************/
+unsigned char find_maximum(char * data, unsigned int size)
+{
+
+
+
+}
+/*************************************************************/
+unsigned char find_minimum(char * data, unsigned int size)
+{
+
+
+
+}
+/*************************************************************/
+void sort_array(char * data, unsigned int size)
+{
+
+
+}
--- stats.h
+++ stats.h
@@ -9,32 +9,99 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.h> 
+ * @brief <this header file has functions to manipulate an array>
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
- *
+ * stats.h is a header file consists of 7 functions' declarations to 
+ * manipulate a data set of 40 unsigned char elements in an array.
+
+ * @author Muhammed Khaled
+ * @date 7:35AM PDT, 10/18/2018
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+#define SIZE (40)
+/***********************************************************************/
 
-/**
- * @brief <Add Brief Description of Function Here>
- *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+void print_statistics(char * data, unsigned int size);
+
+/*print_statistics function prints the mean, median, maximum, and minimum
+ * value in a data set which is an array of characters
+
+ * @param char * data: takes an array name as an input (pass by reference)
+ * @param unsigned int size: takes the size of the input array
+
+ * return type: (void) >> doesn't return anything
  */
+/***********************************************************************/
 
+void print_array(char * data, unsigned int size);
+
+/* print_array function: prints the elements of the array from data[0] to data[size-1] on the screen by looping through the array and print each element at a time.
+
+ * @param char * data: takes an array name as an input (pass by reference)
+ * @param unsigned int size: takes the size of the input array
+
+ * return type: (void) >> doesn't return anything.
+ */
+/************************************************************************/
+
+unsigned char find_median(char * data, unsigned int size);
+
+/*find_median function: returns the median value of a data set (array of character).
+
+ * @param char * data: takes an array name as an input (pass by reference)
+ * @param unsigned int size: takes the size of the input array
+
+ * return type: (char) >> median value of the array.
+ */
+/*********************************************************************/
+
+unsigned char find_mean(char * data, unsigned int size);
+
+/*find_mean function: returns the mean value of a data set (array of characters).
+
+ * @param char * data: takes an array name as an input (pass by reference)
+ * @param unsigned int size: takes the size of the input array
+
+ * return type: (unsigned char) >> mean value of the array.
+ */
+/***********************************************************************/
+
+unsigned char find_maximum(char * data, unsigned int size);
+
+/*find_maximum function: returns the maximum value of a data set (array of character).
+
+ * @param char * data: takes an array name as an input (pass by reference)
+ * @param unsigned int size: takes the size of the input array
+
+ * return type: (unsigned char) >> maximum value of the array.
+ */
+/************************************************************************/
+
+unsigned char find_minimum(char * data, unsigned int size);
+
+/*find_minimum function: returns the minimum value of a data set (array of character).
+
+ * @param char * data: takes an array name as an input (pass by reference)
+ * @param unsigned int size: takes the size of the input array
+
+ * return type: (unsigned char) >> minimum value of the array.
+ */
+/************************************************************************/
+
+void sort_array(char * data, unsigned int size);
+
+/*sort_array function: it's used to sort an array from the largest element to the smallest element, so after sorting:
+ * largest element >>  data[0]
+ * smallest element >> data[size-1]
+
+ * @param char * data: takes an array name as an input (pass by reference)
+ * @param unsigned int size: takes the size of the input array
+
+ * return type: (void) >> doesn't return anything.
+ */
+/*************************************************************************/
 
 #endif /* __STATS_H__ */
